13c8a94ce614e7504c3ef9834fab64c1469c0071
openriceworks <openriceworks@gmail.com>
2023-11-26 16:55:52 +0900

    hlsのプレイリストを取得するAPIを変えた
    * 一度に全ての音声ファイルへのリンクを取得できるAPIに変更した
    * 今まで小分けに三つ区間分の音声ファイルへのリンクしかないAPIを使っていた

--- index.mjs
+++ index.mjs
@@ -1,18 +1,12 @@
 import fetch from 'node-fetch';
-import * as m3u8Parser from "m3u8-parser";
 import {XMLParser}  from 'fast-xml-parser';
 import dayjs from 'dayjs';
-import {setTimeout} from 'timers/promises';
 import fs from 'fs';
 import path from 'path';
 import ffmpeg from 'fluent-ffmpeg'
 
 const DATE_TIME_FORMAT = 'YYYYMMDDhhmmss';
 const DATE_FORMAT = 'YYYYMMDD';
-
-// 一つのファイルの秒数
-const AUDIO_FILE_SECONDS = 5;
-
 const DOWNLOAD_DIR = 'tmp';
 
 /**
@@ -150,182 +144,32 @@ const getMasterPlayList = async (stationId, startAt, endAt, seek = undefined) =>
   params.set('ft', startAt);
   params.set('end_at', endAt);
   params.set('to', endAt);
-  if(seek != null) {
-    params.set('seek', seek)
-  }
   // 固定で大丈夫か?
   params.set('l', '15');
   params.set('lsid', 'a9ed540183dde886192a9095546ae668');
   params.set('type', 'b');
 
-  return `https://tf-f-rpaa-radiko.smartstream.ne.jp/tf/playlist.m3u8?${params.toString()}`
+  return `https://radiko.jp/v2/api/ts/playlist.m3u8?${params.toString()}`
 }
 
-/**
- * ファイルのダウンロード
- * @param {*} url 
- * @param {*} headers 
- * @param {*} fileName 
- * @returns 
- */
-const download = async (url, headers, fileName) => {
-  if(!fs.existsSync(DOWNLOAD_DIR)) {
-    fs.mkdirSync(DOWNLOAD_DIR)
-  }
 
-  const downloadPath = `${DOWNLOAD_DIR}/${fileName}`;
-  if(fs.existsSync(downloadPath)) {
-    // すでにあればダウンロードしない
-    return ;
-  }
+const downloadAudio = async (stationId, startAt, endAt, outputFileName) => {
 
-  const arrayBuffer = await fetch(url, {
-    method: 'GET',
-    headers
-  }).then(res => res.arrayBuffer()).catch(error => {
-    console.error(error);
-    return null;
-  });
-  if(arrayBuffer == null) {
-    throw new Error('download failed!');
-  }
-
-  const buffer = Buffer.from(arrayBuffer);
-  fs.writeFileSync(downloadPath, buffer);
-
-}
-
-/**
- * 番組の音声ファイルをダウンロードする
- * NOTE: 公式サイトのブラウザで使われているAPIを使っているが、他のサンプルコードを見た感じにもう少し簡単に扱えるAPIがありそう
- * @param {*} stationId 
- * @param {*} startAt 
- * @param {*} endAt 
- * @param {*} maxCount 
- * @param {*} seek 
- */
-const downloadAudioList = async (stationId, startAt, endAt, maxCount = undefined, seek = undefined) => {
-
-  if(maxCount == null) {
-    const startTime = dayjs(startAt, DATE_TIME_FORMAT);
-    const endTime = dayjs(endAt, DATE_TIME_FORMAT);
-    // 番組の秒数
-    const totalSeconds = endTime.unix() - startTime.unix();
-    maxCount = totalSeconds / AUDIO_FILE_SECONDS;
-  }
 
   // m3u8ファイルを取得する
   // ただし、m3u8ファイルには「音声ファイルのリンク」はなく、「音声ファイルのリンクへのリンク」が記述されている。
   const { headers }= await authenticate();
-  const playListUrl = await getMasterPlayList(stationId, startAt, endAt, seek);
-  // m3u8ファイルが取れるので解析
-  const m3u8Text = await fetch(playListUrl, {
-    method: 'GET',
-    headers
-  }).then(res => res.text()).catch(error => {
-    console.error(error)
-    return null;
-  })
-  if(m3u8Text == null) {
-    throw new Error(`failed get playlist url. ${playListUrl}`);
-  }
-
-  const parser = new m3u8Parser.Parser();
-  parser.push(m3u8Text.split('\r\n'));
-  parser.end();
-  // 音声ファイルのリンクへのリンク
-  const playListUri = parser.manifest.playlists[0].uri;
-
-  for(let i = 0; i < maxCount; i++) {
-    // 「音声ファイルのリンクへのリンク」に現在時刻(unixtime)のクエリをつけることで、その時間に再生されているであろう音声ファイルへのリンクが取れる
-    const playListUrl = `${playListUri}&_=${dayjs().valueOf()}`;
-    const m3u8Text2 = await fetch(playListUrl, {
-      method: 'GET',
-      headers
-    }).then(res => res.text());
-    const parser2 = new m3u8Parser.Parser();
-    parser2.push(m3u8Text2.split('\r\n'));
-    parser2.end();
-    // 並列ダウンロード
-    await Promise.all(parser2.manifest.segments.map(seg => {
-      const fileName = seg.uri.split('/').reverse()[0];
-      return download(seg.uri, headers, fileName);
-    }));
-
-    await setTimeout(AUDIO_FILE_SECONDS * 1000);
-  }
-}
-
-/**
- * 番組の音声ファイルをダウンロードする（並列実行版）
- * @param {*} stationId 
- * @param {*} startAt 
- * @param {*} endAt 
- * @param {*} downloadRate 
- */
-const downloadAudioListParallel = async (stationId, startAt, endAt, downloadRate = 10) => {
-
-  // 同じ番組を複数再生するように見せかけることで、実時間よりも早くダウンロードする
-
-  const startDateTime = dayjs(startAt, DATE_TIME_FORMAT);
-  const endDateTime = dayjs(endAt, DATE_TIME_FORMAT);
-  
-  // 一分区切り
-  const seekIntervalMinutes = 1;
-  const seekList = [];
-  seekList.push(undefined) // 一番目は時刻指定しないのでundefined
-  let dateTime = startDateTime
-  while(dateTime.isBefore(endDateTime)) {
-    dateTime = dateTime.add(seekIntervalMinutes, 'minute');
-    const seek = dateTime.format(DATE_TIME_FORMAT);
-    seekList.push(seek);
-  }
-  seekList.push(endAt);
-
-  // downloadRateずつのリストに分ける
-  const partitionedSeekList = seekList.reduce((retList, seek) => {
-    if(retList.length > 0 && retList[retList.length - 1].length < downloadRate) {
-      const ret = retList[retList.length - 1];
-      ret.push(seek);
-    } else {
-      retList.push([seek])
-    }
-
-    return retList;
-  }, []);
-
-  console.log(`wait ${partitionedSeekList.length} minutes.`)
-  for(let list of partitionedSeekList) {
-    console.log(`Audio File downloading. Period: ${list[0] ?? startAt} - ${list[list.length - 1]}`)
-    const audioUrlListPromise = list.map(async seek => {
-      const maxCount = seekIntervalMinutes * 60 / AUDIO_FILE_SECONDS;
-      return await downloadAudioList(stationId, startAt, endAt, maxCount, seek);
-    });
-    await Promise.all(audioUrlListPromise);
-    console.log(`Finish. Period: ${list[0] ?? startAt} - ${list[list.length - 1]}`)
-  }
-
-}
-
-/**
- * 複数の音声ファイルを一つにまとめる
- * @param {*} fileDirPath 
- * @param {*} outputFileName 
- */
-const mergeAudioFile = async (fileDirPath, outputFileName) => {
-
-  const fileList = fs.readdirSync(fileDirPath)
-  const fileText = fileList.map(file => `file '${file}'`).join('\n');
-  const listFileName = `${fileDirPath}/list.txt`;
-  fs.writeFileSync(listFileName, fileText);
+  const playListUrl = await getMasterPlayList(stationId, startAt, endAt, undefined);
+  console.log(`downloading. ${playListUrl}`);
 
   ffmpeg()
-    .input(listFileName)
-    .inputOptions(['-f concat', '-safe 0'])
-    .outputOptions('-c copy')
+    .input(playListUrl)
+    .inputOption('-headers', `X-Radiko-Authtoken: ${headers['x-radiko-authtoken']}`)
     .output(outputFileName)
+    .on('end', () => {
+      console.log(`finished. ${path.resolve(outputFileName)}`);
+    })
     .run();
-
 }
 
 const main = async (url) => {
@@ -351,19 +195,11 @@ const main = async (url) => {
   }
   fs.mkdirSync(DOWNLOAD_DIR);
 
-  console.log('Audio File downloading.');
-  await downloadAudioListParallel(program.stationId, program.startAt, program.endAt);
-  console.log('All Audio File downloaded.');
-
-  // TODO とりあえずm4a固定にしたけど、ffmpegで使える拡張子に対応させる
   // ファイル名
   const startDateStr = dayjs(program.startAt, DATE_TIME_FORMAT).format('YYYY年MM月DD日')
-  const outputFileName = `${program.title}_${startDateStr}.m4a`
+  const outputFileName = `${DOWNLOAD_DIR}/${program.title}_${startDateStr}.wav`
 
-  // ダウンロードした音声ファイルを結合させる
-  mergeAudioFile(DOWNLOAD_DIR, outputFileName);
-  console.log(`complete! ${path.resolve(outputFileName)}`);
-
+  await downloadAudio(program.stationId, program.startAt, program.endAt, outputFileName)
 }
 // radikoの再生画面のURL(https://radiko.jp/#!/ts/STATION_ID/YYYYMMDDhhmmss のような)
 const programUrl = process.argv[2] // TODO 決め打ちなので、修正する
